ff9e35758dd3bba2cd1a23a71749cd627c08718a
liam <lmcintyre995@gmail.com>
2019-04-09 12:40:45 -0400

    fix persistence query

--- app/src/main/java/org/team7/notificationlog/DBNotificationDao.java
+++ app/src/main/java/org/team7/notificationlog/DBNotificationDao.java
@@ -17,10 +17,10 @@ public interface DBNotificationDao {
     @Query("SELECT * FROM notifications")
     List<DBNotification> getAll();
 
-    @Query("SELECT * FROM notifications WHERE ongoing = 0 OR noclear = 0")
+    @Query("SELECT * FROM notifications WHERE ongoing = 0 AND noclear = 0")
     LiveData<List<DBNotification>> getAllNonPersistentLive();
 
-    @Query("SELECT * FROM notifications WHERE ongoing = 0 OR noclear = 0")
+    @Query("SELECT * FROM notifications WHERE ongoing = 0 AND noclear = 0")
     List<DBNotification> getAllNonPersistent();
 
     //TODO: consider building exclusion into the query?
